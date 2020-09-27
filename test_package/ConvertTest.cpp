a4934c1eba9cff75d6b69d1f7aee82fc871f68fa
daixian <amano_tooko@qq.com>
2020-09-27 20:51:06 +0800

    增加两个Test

--- test_package/ConvertTest.cpp
+++ test_package/ConvertTest.cpp
@@ -51,6 +51,7 @@ TEST(Convert, Base32)
 {
     vector<unsigned char> data;
 
+    //从0字节到1024字节的长度依次
     for (size_t count = 0; count < 1024; count++) {
         data.clear();
         for (size_t i = 0; i < count; i++) {
@@ -60,6 +61,7 @@ TEST(Convert, Base32)
         string base32 = Convert::ToBase32String(data.data(), 0, data.size());
         vector<unsigned char> data2 = Convert::FromBase32String(base32);
 
+        ASSERT_TRUE(data.size() == data2.size());
         for (size_t i = 0; i < data.size(); i++) {
             ASSERT_TRUE(data[i] == data2[i]);
         }
@@ -84,5 +86,22 @@ TEST(Convert, Base32_2)
         for (size_t i = 0; i < data2.size(); i++) {
             ASSERT_TRUE(data[i + offset] == data2[i]);
         }
+    }
+}
+
+TEST(Convert, Base32_12byte)
+{
+    //https://cryptii.com/pipes/crockford-base32
+    vector<unsigned char> data;
+
+    for (size_t i = 0; i < 2; i++) {
+        data.push_back(255);
+    }
+
+    string base32 = Convert::ToBase32String(data.data(), 0, data.size());
+    vector<unsigned char> data2 = Convert::FromBase32String(base32);
+
+    for (size_t i = 0; i < data.size(); i++) {
+        ASSERT_TRUE(data[i] == data2[i]);
     }
 }
